b5f11dedbf8ea0afbc2cc3347d15fe311320c5db
abertuccio <andresbertuccio@gmail.com>
2019-08-02 11:22:49 -0300

    find firs translation

--- content/translation/translation.js
+++ content/translation/translation.js
@@ -8,6 +8,59 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 });
 
 
+const searchData = function () {
+
+    const element = document.querySelectorAll(".tlid-translation.translation")[0];
+
+    var results = {};
+
+    results.search = decodeURIComponent(window.location.href.split("=")[5]);
+
+    results.translations = [element.innerText]
+
+
+    var otherTranslations = document.querySelectorAll(".gt-baf-cell.gt-baf-word-clickable");
+
+    var transliteration = document.querySelectorAll(".tlid-transliteration-content.transliteration-content.full");
+
+    //TODO: VER QUE ESTO NO FUNCIONA
+    if (transliteration.length && transliteration[0].innerText.length) {
+        results.transliteration = transliteration[0].innerText;
+    } else {
+        results.transliteration = "";
+    }
+
+    if (otherTranslations.length) {
+        [...otherTranslations].forEach(e => {
+            if (/,/g.test(e.innerText)) {
+                e.innerText.split(",").forEach(sub => {
+                    results.translations.push(sub);
+                })
+            } else {
+                results.translations.push(e.innerText);
+            }
+        })
+    }
+
+    // setTimeout(()=>{
+
+    var defifitionGuess = document.querySelectorAll(".gt-def-row");
+
+    results.definitions = (defifitionGuess.length) ? [...defifitionGuess].map(e => e.innerText) : [""];
+
+    chrome.runtime.sendMessage({
+        target: 'background',
+        action: 'SEND_INFORMATION',
+        kind: 'translation',
+        selection: results.search,
+        result: results
+    });
+
+    [...defifitionGuess].forEach(d => { d.remove() });
+}
+
+
+
 const mutateTranslation = (mutations) => {
 
     for (let i = 0; i < [...mutations].length - 1; i++) {
@@ -18,55 +71,7 @@ const mutateTranslation = (mutations) => {
         //TODO:NO ES UNA BUENA FORMA DE ELEGIR EL MUTADO
         if (!/\.\.\./g.test(element.innerText)) {
 
-            var results = {};
-
-            results.search = decodeURIComponent(window.location.href.split("=")[5]);
-
-            results.translations = [element.innerText]
-
-            var otherTranslations = document.querySelectorAll(".gt-baf-cell.gt-baf-word-clickable");
-
-            var transliteration = document.querySelectorAll(".tlid-transliteration-content.transliteration-content.full");
-
-            //TODO: VER QUE ESTO NO FUNCIONA
-            if (transliteration.length && transliteration[0].innerText.length) {
-                results.transliteration = transliteration[0].innerText;
-            } else {
-                results.transliteration = "";
-            }
-
-            if (otherTranslations.length) {
-                [...otherTranslations].forEach(e => {
-                    if (/,/g.test(e.innerText)) {
-                        e.innerText.split(",").forEach(sub => {
-                            results.translations.push(sub);
-                        })
-                    } else {
-                        results.translations.push(e.innerText);
-                    }
-                })
-            }
-
-            // setTimeout(()=>{
-
-                var defifitionGuess = document.querySelectorAll(".gt-def-row");
-
-                // console.log(defifitionGuess);
-    
-                results.definitions = (defifitionGuess.length) ? [...defifitionGuess].map(e => e.innerText) : [""];
-    
-                chrome.runtime.sendMessage({
-                    target: 'background',
-                    action: 'SEND_INFORMATION',
-                    kind: 'translation',
-                    selection: results.search,
-                    result: results
-                });
-
-                [...defifitionGuess].forEach(d=>{d.remove()});
-
-            // },100)
-
+            searchData();
 
             break;
 
@@ -76,6 +81,12 @@ const mutateTranslation = (mutations) => {
 
 
 var targetTranslation = document.querySelectorAll(".tlid-results-container.results-container")[0];
+
+var search = decodeURIComponent(window.location.href.split("=")[5]);
+var actualSearch = document.getElementsByClassName("text-dummy")[0];
+searchData();
 var observerTranslation = new MutationObserver(mutateTranslation);
 var configTranslation = { characterData: false, attributes: true, childList: false, subtree: false };
 observerTranslation.observe(targetTranslation, configTranslation);
+
+
