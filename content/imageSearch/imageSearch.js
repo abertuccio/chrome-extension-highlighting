04c8ed9f765ca81d200d7ea869edf48908566753
abertuccio <abertuccio@gmail.com>
2020-02-26 10:29:55 -0300

    stored image bug

--- content/imageSearch/imageSearch.js
+++ content/imageSearch/imageSearch.js
@@ -8,18 +8,18 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         const imageSearchURL = `https://www.google.com/search?q=${request.selection}&source=lnms&tbm=isch`;
         window.location.href = imageSearchURL;
     }
-    if (request.target === 'images' && request.action === 'ASK_LARGER_IMAGE_LINK') {        
+    if (request.target === 'images' && request.action === 'ASK_LARGER_IMAGE_LINK') {
         // document.querySelectorAll(".rg_ic.rg_i")[metaInfo[request.index]].click(); 
 
 
-        console.log(request.id); 
-        console.log(request.idx); 
+        const id = request.idx ? request.idx : 0;
 
-        if(!request.id && !request.id){
+        console.log("------");
+        console.log(id);
+        console.log("------");
 
-            [...document.querySelectorAll(".rg_i.Q4LuWd.tx8vtf")][0].click();
+        [...document.querySelectorAll(".rg_i.Q4LuWd.tx8vtf")][id].click();
 
-        }
 
         // [...document.querySelectorAll(".rg_i.Q4LuWd.tx8vtf")][request.idx].click();
 
@@ -28,26 +28,26 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 
         setInterval(() => {
-            let candidates = [...document.getElementsByClassName("rg_i")];
-            
-            if(candidates[1].src){
-                sendResponse(candidates[1].src);                
+            let candidates = [...document.getElementsByClassName("n3VNCb")];
+
+            if (candidates[1].src) {
+                sendResponse(candidates[1].src);
             }
-            else{
-                for(let i = 0; i < candidates.length -1; i++){
-                    if(candidates[i].src){
+            else {
+                for (let i = 0; i < candidates.length - 1; i++) {
+                    if (candidates[i].src) {
                         sendResponse(candidates[i].src);
                         break;
                     }
                 }
             }
-        }, 500);  
+        }, 500);
     }
     return true;
 });
 
 const mutateImages = (mutations) => {
-    
+
     if (mutations.length > 0) {
 
         //TODO: GUARDAR ESTAS IMAGENES AFUERA CUANDO EL USUARIO ELIJA GUARDAR EL 
@@ -56,11 +56,11 @@ const mutateImages = (mutations) => {
         //quiza hay que esperar unos milisegundos
         //document.getElementsByClassName("irc_mi")[0, 1 o 2].currentSrc
         imgs = [...document.querySelectorAll(".rg_i.Q4LuWd.tx8vtf")].reduce((a, c, i) => {
-            if (c.src) { 
-                a.push(c.src); 
+            if (c.src) {
+                a.push(c.src);
                 ids.push(c.id);
                 idxs.push(i);
-            } 
+            }
             return a;
         }, [])
 
@@ -72,7 +72,7 @@ const mutateImages = (mutations) => {
             selection: decodeURIComponent(window.location.href.split("=")[1].split("&")[0]),
             result: imgs,
             ids: ids,
-            idxs:idxs
+            idxs: idxs
         });
 
         observerImages.disconnect();
